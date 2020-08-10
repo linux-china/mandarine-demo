4ea4581ba3b903dc1380d10063a086433b4ca9a4
linux_china <libing.chen@gmail.com>
2020-08-10 16:13:48 -0700

    MyController

---
+++ src/main/mandarine/controllers/MyController.ts
@@ -0,0 +1,19 @@
+import {Controller, GET, Inject} from "../deps.ts";
+import {ManualInjectionService} from "../services/ManualInjectionService.ts";
+
+@Controller()
+export class MyController {
+    @Inject()
+    public manualInjectionService: ManualInjectionService;
+
+    @GET('/welcome')
+    public async httpHandler() {
+        return Promise.resolve("Welcome to MandarineTS Framework!");
+    }
+
+    @GET('/helloWorld')
+    public helloWorld() {
+        return this.manualInjectionService.helloWorld();
+    }
+
+}
