c582d2aa81b695f9c9522eb51a726166e25f923e
linux_china <libing.chen@gmail.com>
2020-08-10 16:54:55 -0700

    polishing

--- src/main/mandarine/deps.ts
+++ src/main/mandarine/deps.ts
@@ -1,6 +1,6 @@
 export {
     MandarineCore,
-    Configuration, Service,
+    Configuration, Service, Component,
     Injectable, Inject,
     Controller, GET
 } from "https://x.nest.land/MandarineTS@1.5.0/mod.ts";
