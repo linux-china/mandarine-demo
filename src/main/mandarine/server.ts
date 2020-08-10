99c2c43823d8cf0f633a1b1543671bf094e7c7b3
linux_china <libing.chen@gmail.com>
2020-08-10 16:55:15 -0700

    polishing

--- src/main/mandarine/server.ts
+++ src/main/mandarine/server.ts
@@ -1,17 +1,10 @@
 import {Configuration, Injectable, MandarineCore} from "./deps.ts";
 
 import {MyController} from "./controllers/MyController.ts";
-import {ManualInjectionService} from "./services/ManualInjectionService.ts";
 
 @Configuration()
 export class MainConfig {
 
-    @Injectable()
-    public ManualInjectionService() {
-        return new ManualInjectionService("Bill");
-    }
-
-    @Injectable()
     public MyController() {
         return new MyController();
     }
