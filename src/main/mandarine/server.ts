64a9c6de3d92cd5beb4b24915dafba35b2210d01
linux_china <libing.chen@gmail.com>
2020-08-10 16:13:38 -0700

    add configuration

--- src/main/mandarine/server.ts
+++ src/main/mandarine/server.ts
@@ -1,7 +1,21 @@
-import {MandarineCore} from "./deps.ts";
+import {Configuration, Injectable, MandarineCore} from "./deps.ts";
 
-import {MyController} from "./MyController.ts";
+import {MyController} from "./controllers/MyController.ts";
+import {ManualInjectionService} from "./services/ManualInjectionService.ts";
 
-new MyController();
+@Configuration()
+export class MainConfig {
+
+    @Injectable()
+    public ManualInjectionService() {
+        return new ManualInjectionService("Bill");
+    }
+
+    @Injectable()
+    public MyController() {
+        return new MyController();
+    }
+
+}
 
 new MandarineCore().MVC().run();
