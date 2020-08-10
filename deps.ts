f7b61c6d7ffb7a9d46c504cfc947fbda87cd84bc
linux_china <libing.chen@gmail.com>
2020-08-10 16:13:24 -0700

    dep

--- deps.ts
+++
@@ -1,1 +0,0 @@
-export { MandarineCore, Controller, GET } from "https://x.nest.land/MandarineTS@1.5.0/mod.ts";
---
+++ src/main/mandarine/deps.ts
@@ -0,0 +1,6 @@
+export {
+    MandarineCore,
+    Configuration, Service,
+    Injectable, Inject,
+    Controller, GET
+} from "https://x.nest.land/MandarineTS@1.5.0/mod.ts";
