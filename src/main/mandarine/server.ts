51c7d993817738cc441370c546b45d2a57559164
linux_china <libing.chen@gmail.com>
2020-08-10 21:02:01 -0700

    polishing

--- src/main/mandarine/server.ts
+++ src/main/mandarine/server.ts
@@ -5,7 +5,11 @@ import {ManualInjectionService} from "./services/ManualInjectionService.ts"
 
 // Mandarine.TS Components Map to load all components
 const componentsMap = {
+    configurations: [],
+    repositories: [],
     services: [ManualInjectionService],
+    middlewares: [],
+    components: [],
     controllers: [MyController]
 };
 
