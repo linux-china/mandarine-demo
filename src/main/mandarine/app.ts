2209f057cb5a1ff9623226bde0bdce19ce089715
linux_china <libing.chen@gmail.com>
2020-08-16 17:52:41 -0700

    add AppConfiguration

--- src/main/mandarine/app.ts
+++ src/main/mandarine/app.ts
@@ -2,11 +2,12 @@ import {MandarineCore} from "./deps.ts";
 
 import {MyController} from "./controllers/MyController.ts"
 import {ManualInjectionService} from "./services/ManualInjectionService.ts"
+import {AppConfiguration} from "./AppConfiguration.ts";
 
 // Mandarine.TS Components Map to load all components
 // noinspection JSUnusedLocalSymbols
 const componentsMap = {
-    configurations: [],
+    configurations: [AppConfiguration],
     repositories: [],
     services: [ManualInjectionService],
     middlewares: [],
