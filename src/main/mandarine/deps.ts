8e436870a7a315ecefe21f7405b28debb202255a
linux_china <libing.chen@gmail.com>
2020-08-16 17:52:49 -0700

    add UserService

--- src/main/mandarine/deps.ts
+++ src/main/mandarine/deps.ts
@@ -4,4 +4,7 @@ export {
     Injectable, Inject,
     Controller, GET, POST,
     Render, Model, ViewModel,
+    RouteParam, QueryParam, RequestParam
 } from "https://x.nest.land/MandarineTS@1.5.0/mod.ts";
+
+export {UserService} from "./AppConfiguration.ts"
