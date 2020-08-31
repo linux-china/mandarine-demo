86c8b54333c04a9ee1e5fe5c377ddcd68ef4c56f
linux_china <libing.chen@gmail.com>
2020-08-31 14:52:12 -0700

    update to MandarineTS 2.0.0

--- src/main/mandarine/deps.ts
+++ src/main/mandarine/deps.ts
@@ -5,6 +5,6 @@ export {
     Controller, GET, POST,
     Render, Model, ViewModel,
     RouteParam, QueryParam, RequestParam
-} from "https://x.nest.land/MandarineTS@1.5.0/mod.ts";
+} from "https://x.nest.land/MandarineTS@2.0.0/mod.ts";
 
 export {UserService} from "./configurations.ts"
