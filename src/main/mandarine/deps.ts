98b76fa6ed00e1d81beec92ff8d00b857dab4555
linux_china <libing.chen@gmail.com>
2020-08-31 17:45:49 -0700

    add ValueScopes

--- src/main/mandarine/deps.ts
+++ src/main/mandarine/deps.ts
@@ -1,4 +1,5 @@
 export {
+    Mandarine,
     MandarineCore, MandarineMVC,
     Configuration, Service, Component, Value,
     Injectable, Inject,
@@ -6,5 +7,9 @@ export {
     Render, Model, ViewModel,
     RouteParam, QueryParam, RequestParam
 } from "https://x.nest.land/MandarineTS@2.0.0/mod.ts";
+
+import {Mandarine} from "https://x.nest.land/MandarineTS@2.0.0/mod.ts";
+
+export const ValueScopes = Mandarine.MandarineCore.ValueScopes;
 
 export {UserService} from "./configurations.ts"
