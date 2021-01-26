7f200fa8a0470d3ef23d132fb4c9978f6dc4af2d
linux_china <libing.chen@gmail.com>
2021-01-25 17:38:10 -0800

    Update to Mandarine 2.3.2

--- src/main/mandarine/deps.ts
+++ src/main/mandarine/deps.ts
@@ -6,8 +6,8 @@ export {
     Controller, GET, POST,
     Render, Model, ViewModel,
     RouteParam, QueryParam, RequestParam
-} from "https://deno.land/x/mandarinets@v2.3.0/mod.ts";
+} from "https://deno.land/x/mandarinets@v2.3.2/mod.ts";
 
-import {Mandarine} from "https://deno.land/x/mandarinets@v2.3.0/mod.ts";
+import {Mandarine} from "https://deno.land/x/mandarinets@v2.3.2/mod.ts";
 
 export const ValueScopes = Mandarine.MandarineCore.ValueScopes;
